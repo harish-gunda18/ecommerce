cb2f93ef322486546fa4f3fec0ce5c17273867a1
sgunda <harish.gunda16@gmail.com>
2020-06-29 13:26:05 +0530

    adding orderiteminline to admin

--- order/admin.py
+++ order/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import Item, Order, OrderItem
 
 
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderItemInline,
+    ]
+
+
 admin.site.register(Item)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderItem)
