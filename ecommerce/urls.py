0099797b5d31de540f02fe6ef00ace2dbbca8202
sgunda <harish.gunda16@gmail.com>
2020-07-22 15:43:08 +0530

    adding paypal urls

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('order.urls')),
+    path('paypal/', include('paypal.standard.ipn.urls')),
 ]
